47f582eefc3aa09adf8139fac10d9d2f27416759
merprandi <126601191+merprandi@users.noreply.github.com>
2023-03-20 06:25:23 -0300

    Delete js directory

--- js/funcion.js
+++
@@ -1,9 +0,0 @@
-/**document.getElementById('boton').onclick = function () {
-    console.log("Capturamos el evento click");
-    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
-}
-*/
-document.addEventListener('click', function() {
-    console.log("Hola mundo desde EventListener");
-    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en JS"
-})
